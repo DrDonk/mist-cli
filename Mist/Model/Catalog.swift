9a622f32df45bad1e9e363f7f5793e46b57baa61
David Parsons <869796+DrDonk@users.noreply.github.com>
2025-07-13 10:36:23 +0100

    macOS 26 Tahoe Support

--- Mist/Model/Catalog.swift
+++ Mist/Model/Catalog.swift
@@ -12,7 +12,7 @@ enum Catalog: String, CaseIterable {
     case `public`
 
     static var urls: [String] {
-        allCases.map(\.url) + allCases.map(\.sequoiaURL)
+        allCases.map(\.url) + allCases.map(\.tahoeURL)
     }
 
     var url: String {
@@ -31,9 +31,9 @@ enum Catalog: String, CaseIterable {
     private var sequoiaURL: String {
         switch self {
         case .standard:
-            "https://swscan.apple.com/content/catalogs/others/index-15-14-13-12-10.16-10.15-10.14-10.13-10.12-10.11-10.10-10.9-mountainlion-lion-snowleopard-leopard.merged-1.sucatalog.gz"
-        case .customer: // swiftlint:disable:next line_length
-            "https://swscan.apple.com/content/catalogs/others/index-15customerseed-15-14-13-12-10.16-10.15-10.14-10.13-10.12-10.11-10.10-10.9-mountainlion-lion-snowleopard-leopard.merged-1.sucatalog.gz"
+            "https://swscan.apple.com/content/catalogs/others/index-26-15-14-13-12-10.16-10.15-10.14-10.13-10.12-10.11-10.10-10.9-mountainlion-lion-snowleopard-leopard.merged-1.sucatalog.gz"
+        case .customer:  // swiftlint:disable:next line_length
+            "https://swscan.apple.com/content/catalogs/others/index-26customerseed-26-15-14-13-12-10.16-10.15-10.14-10.13-10.12-10.11-10.10-10.9-mountainlion-lion-snowleopard-leopard.merged-1.sucatalog.gz"
         case .developer:
             "https://swscan.apple.com/content/catalogs/others/index-15seed-15-14-13-12-10.16-10.15-10.14-10.13-10.12-10.11-10.10-10.9-mountainlion-lion-snowleopard-leopard.merged-1.sucatalog.gz"
         case .public:
