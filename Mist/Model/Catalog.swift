e55be0ff832468b6552f2ca65838da3fde5062d4
David Parsons <869796+DrDonk@users.noreply.github.com>
2025-07-13 10:38:09 +0100

    Update Catalog.swift

--- Mist/Model/Catalog.swift
+++ Mist/Model/Catalog.swift
@@ -28,7 +28,7 @@ enum Catalog: String, CaseIterable {
         }
     }
 
-    private var sequoiaURL: String {
+    private var tahoeURL: String {
         switch self {
         case .standard:
             "https://swscan.apple.com/content/catalogs/others/index-26-15-14-13-12-10.16-10.15-10.14-10.13-10.12-10.11-10.10-10.9-mountainlion-lion-snowleopard-leopard.merged-1.sucatalog.gz"
